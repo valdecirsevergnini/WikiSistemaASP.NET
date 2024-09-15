fc6e44d326d174b49cf32db192cd1c13067ffdda
VALDECIR SEVERGNINI <valdecirsevergnini@gmail.com>
2024-09-15 19:05:32 -0300

    error de duplicação

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WikiSistemaASP.NET.Models;
-using WikiSistemaASP.NET.Data;
 
 namespace WikiSistemaASP.NET.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly WikiDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, WikiDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
         public IActionResult Index()
         {
-            var topicos = _context.Topicos.ToList(); // Supondo que você tenha uma tabela Topicos
-            return View(topicos);
+            return View();
         }
 
         public IActionResult Privacy()
