07aae299be12255e0a43a638bac39aed8d08c864
VALDECIR SEVERGNINI <valdecirsevergnini@gmail.com>
2024-09-15 22:43:48 -0300

    Atualizando arquivos e configurando o projeto

--- Models/ApllicationDbContext.cs
+++ Models/ApllicationDbContext.cs
@@ -11,5 +11,19 @@ namespace WikiSistemaASP.NET.Models
 
         // Adicione suas entidades aqui
         public DbSet<YourEntity> YourEntities { get; set; }
+        public DbSet<Topico> Topicos { get; set; }
+        public DbSet<Modulo> Modulos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        // Configurações adicionais, se necessário
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configurações específicas para suas entidades, se necessário
+            // Exemplo: modelBuilder.Entity<YourEntity>().ToTable("YourEntities");
+
+            // Adicione outras configurações se necessário
+        }
     }
 }
--- obj/Debug/net8.0/WikiSistemaASP.NET.AssemblyInfo.cs
+++ obj/Debug/net8.0/WikiSistemaASP.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiSistemaASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1e0988e23366b5c1e2c4140b9e510c99db96ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiSistemaASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiSistemaASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
